a7e07dd5617036795f534792e466d4ff770e4da9
search <samaatarek12z@gmail.com>
2025-05-07 22:36:10 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var typed = new Typed("#typed", {
+  strings: ["I Am Developer", " I Am Lory Danial ", " I Am designer"],
+  typeSpeed: 50,
+  backSpeed: 5,
+  loop: true,
+  showCursor: true,
+  cursorChar: "|",
+});
+
+
+
+
+
+
